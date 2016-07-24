10d5e5face7ab21d725f1618d8ae4921fe0c5a92
NicolasMekgwe <1126619@students.wits.ac.za>
2016-07-24 17:33:31 +0200

    first commit

--- hw0/wc.c
+++ hw0/wc.c
@@ -8,9 +8,27 @@ void wc ( FILE *infile, char *inname);
 int
 main (int argc, char *argv[]) {
     
+    
 }
 
 void
 wc( FILE *infile, char *inname) {
+    int count=0;
+
+file = fopen(FILE_READ, "r");
+char w;
+
+while((w = fget(infile)) != EOF){
+
+    if( w != '' && w != \n){
+        words++;
+    }
+    if( w != '' && w== \n){
+        lines++;
+    }
+    if( w != \n && w == ''){
+        spaces++;
+    }
+    
     
 }
